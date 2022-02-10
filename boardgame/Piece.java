a85cdcbb5488305ae68dc6758fa051d2d1859efd
Daniel <daniel0202almeida@outlook.com>
2022-02-10 15:13:05 -0300

    Printing possible moves

--- boardgame/Piece.java
+++ boardgame/Piece.java
@@ -20,14 +20,14 @@ public abstract class Piece {
         return board;
     }
 
-    public abstract boolean[][] posibleMoves();
+    public abstract boolean[][] possibleMoves();
 
-    public boolean posibleMove(Position position){
-        return posibleMoves()[position.getRow()][position.getColumn()];
+    public boolean possibleMove(Position position){
+        return possibleMoves()[position.getRow()][position.getColumn()];
     }
   
     public boolean isThereAnyPossibleMove(){
-        boolean[][] mat = posibleMoves();
+        boolean[][] mat = possibleMoves();
         for(int i = 0; i < mat.length; i++){
             for(int j = 0; j < mat.length; j++){
                 if(mat[i][j]){
